4d9c1f65fd213621a6df884b0119c58bbd28ccbf
Sushma Badiger <54473849+Sushma-pb@users.noreply.github.com>
2022-09-09 07:37:24 +0530

    Add files via upload

---
+++ Front End Technologies/JAVA SCRIPT/JS Files/1. JavaScript Basics/basics4.js
@@ -0,0 +1,12 @@
+const person = {
+    firstName: "Sushma",
+    lastName: "Badiger",
+  };
+  // firstWay
+  console.log(person.firstName);
+  console.log(person.lastName);
+  
+  //secondWay
+  console.log(person["firstName"]);
+  console.log(person["lastName"]);
+  
